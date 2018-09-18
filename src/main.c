50a9373df3ccaa66dfeed8387328bca1f68329dd
danbailo <danbailoufms@gmail.com>
2018-09-17 23:34:23 -0400

    organizei a main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-// #include <stdlib.h>
 #include "dbPoint.h"
 #include "dbVertex.h"
 #include "dbEdge.h"
@@ -11,20 +10,15 @@
 
 int main(int argc, char *argv[]){
 
-    printf("\nPoints\n");
 	dbSurface* surface = getSurface("points.txt","vertices.txt");
-
-	/* no caso se eu fosse receber a hash de pontos do tipo ponto */
-	// dbSurface *OIIII = (dbSurface*)malloc(sizeof(dbSurface));
-	// OIIII->Points= POINTS_HASH;
-
+    
+    printf("\nPoints\n");
 	for(dbPoint* p = surface->Points; p != NULL; p=(dbPoint*)(p->hh.next)) {
         printf("%lf %lf %lf\n",p->x,p->y,p->z);
     }
 
+
     printf("\nVertices\n");
-
-
 	for(dbVertex* v = surface->Vertices; v != NULL; v=(dbVertex*)(v->hh.next)) {
         printf("%.lf %.lf %.lf\n",v->v1,v->v2,v->v3);
     }
